2d43644b959c3062e074f4d05b86f0e504093d8b
Ayush Singh <ayushpratap251@gmail.com>
2026-07-31 21:57:17 +0530

    Some Pytest, some PostgresSQL basic command prac

---
+++ Python/Projects/test_CLI_Basic_Calculator.py
@@ -0,0 +1,10 @@
+from CLI_Basic_Calculator import add, divide
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10, 0)
